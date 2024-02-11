3e461707c225666c2b153c36d9fbf06623029078
Mironov Semen <mobrick@bk.ru>
2024-02-11 21:48:23 +0300

    fixed connection flag

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,48 +1,48 @@
 package config
 
 import (
-    "flag"
-    "os"
+	"flag"
+	"os"
 )
 
 type Config struct {
-	FlagRunAddr string
-    FlagShortURLBaseAddr string
-    FlagLogLevel string
-    FlagFileStoragePath string
-    FlagDBConnectionAddress string
+	FlagRunAddr             string
+	FlagShortURLBaseAddr    string
+	FlagLogLevel            string
+	FlagFileStoragePath     string
+	FlagDBConnectionAddress string
 }
 
 func MakeConfig() *Config {
-    config := &Config{}
+	config := &Config{}
 
 	flag.StringVar(&config.FlagRunAddr, "a", ":8080", "address to run server")
 	flag.StringVar(&config.FlagShortURLBaseAddr, "b", "http://localhost:8080/", "base address of shortened URL")
 	flag.StringVar(&config.FlagLogLevel, "l", "info", "log level")
-    flag.StringVar(&config.FlagFileStoragePath, "f", "tmp/short-url-db.json", "path of file with saved URLs")
-    flag.StringVar(&config.FlagDBConnectionAddress, "d", "", "database connection address")
+	flag.StringVar(&config.FlagFileStoragePath, "f", "tmp/short-url-db.json", "path of file with saved URLs")
+	flag.StringVar(&config.FlagDBConnectionAddress, "d", "host=localhost dbname=URLRecords sslmode=disable", "database connection address")
 
 	flag.Parse()
 
 	if envRunAddr := os.Getenv("SERVER_ADDRESS"); envRunAddr != "" {
-        config.FlagRunAddr = envRunAddr
-    }
+		config.FlagRunAddr = envRunAddr
+	}
 
 	if envBaseAddr := os.Getenv("BASE_URL"); envBaseAddr != "" {
-        config.FlagShortURLBaseAddr = envBaseAddr
-    }
+		config.FlagShortURLBaseAddr = envBaseAddr
+	}
 
-    if envLogLevel := os.Getenv("LOG_LEVEL"); envLogLevel != "" {
-        config.FlagLogLevel = envLogLevel
-    }
+	if envLogLevel := os.Getenv("LOG_LEVEL"); envLogLevel != "" {
+		config.FlagLogLevel = envLogLevel
+	}
 
-    if envFileStoragePath := os.Getenv("FILE_STORAGE_PATH"); envFileStoragePath != "" {
-        config.FlagFileStoragePath = envFileStoragePath
-    }
+	if envFileStoragePath := os.Getenv("FILE_STORAGE_PATH"); envFileStoragePath != "" {
+		config.FlagFileStoragePath = envFileStoragePath
+	}
 
-    if envDBConnectionAddress := os.Getenv("DATABASE_DSN"); envDBConnectionAddress != "" {
-        config.FlagDBConnectionAddress = envDBConnectionAddress
-    }
+	if envDBConnectionAddress := os.Getenv("DATABASE_DSN"); envDBConnectionAddress != "" {
+		config.FlagDBConnectionAddress = envDBConnectionAddress
+	}
 
-    return config
+	return config
 }
